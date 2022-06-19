425ae3f5adbc3ef2c47f0da142abe2d6bd4747bc
Kanak-dev <kanak24.fv@gmail.com>
2022-06-19 13:24:20 +0545

    readme file updated

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # portfolio-site
+
+# TITLES 
+## Brief info on me
+### Biography
+### Contact
+### Services
+### Years of Experience(experience with HTML5, CSS3, and JS, not with job)
+### Satisfaction clients (I don't have client yet)
+### Clients on worldwide (I don't have client yet)
+### Projects Done
+
+## My advantages(skills)
+## Education & Experience
+## Projects
+## CV
+## Contact
+
+## Introduction
+My portfolio site project that describes me well. It depicts my skills, educational background, experiences & some of my projects.
+
+## Technologies
+- HTML5
+- CSS3
+- JS
+- JQuery
+
+## Launch
+Go to 
--- site.js
+++ site.js
@@ -3,20 +3,37 @@ $(document).ready(function() {
                         },
                          2500)});
 
+let offset_value = 100;
+
+$(document).ready(function() {
+    $(window).scroll(function () { 
+        scrollFunction();
+    });
+})
+
+function scrollFunction() {
+    console.log(opacity)
+    if(window.pageYOffset > offset_value) {
+        $('.scroll_up').fadeIn(3000);
+        $('.scroll_up').css({'display': 'block'});
+    }
+    else $('.scroll_up').fadeOut();
+}
+
 // owl carousel script
 $(document).ready(function() {
-    $(".project_container").owlCarousel({
-        margin: 30, // sets the margin of the '.profile' to 20px    
+    $(".owl-carousel").owlCarousel({
+        margin: 30,     // sets the margin of the '.project' to 30px    
         loop: true,     // loop is activated
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
            0: {
-               items: 1,    //only a team-mate is shown on the page
+               items: 1,    //only a project is shown on the page
                nav: false
            }, 
            600: {
-                items: 2,       //only 2 team-mates are shown
+                items: 2,       //only 2 projects are shown
                 nav: false
             }, 
             945: {
@@ -26,16 +43,3 @@ $(document).ready(function() {
         }
     })
 })
-
-// const projectImage = document.querySelectorAll('.ss');
-// // projectImage.forEach(changeImage)
-
-// function changeImage(imgArr) {
-//     index = 0;
-//     projectImage = imgArr[index];
-//     if(index === 0) index++;
-    
-// }
-
-// const personal_site_img = ['images/personal_site_ss.jpg', 'images/personal_site_ss_dark.jpg'];
-// const snake_game = ['images/snake_game_menu_ss.jpg', 'images/snake_game_ss.jpg'];
